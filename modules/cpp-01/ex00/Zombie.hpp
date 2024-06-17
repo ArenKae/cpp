abc3c1c89bf46882e149288bce9f3453ace5f133
acosi <cosianthony@gmail.com>
2024-06-17 16:01:06 +0200

    Starting cpp01

---
+++ modules/cpp-01/ex00/Zombie.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acosi <acosi@student.42nice.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/17 15:02:24 by acosi             #+#    #+#             */
+/*   Updated: 2024/06/17 15:10:38 by acosi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_H
+#define ZOMBIE_H
+
+#include <iostream>
+
+class Zombie
+{
+	private:
+		std::string name;
+	public:
+		Zombie(void);
+		~Zombie(void);
+		void announce(void);
+};
+
+#endif

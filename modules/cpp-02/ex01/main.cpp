f7ae6eece10dd16304a574e45136aacc5cd0443b
acosi <acosi@c1r1p3.42nice.fr>
2024-07-01 02:01:05 +0200

    ex00 ok

--- modules/cpp-02/ex01/main.cpp
+++ modules/cpp-02/ex01/main.cpp
@@ -1,35 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acosi <acosi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/30 23:12:05 by acosi             #+#    #+#             */
-/*   Updated: 2024/07/01 01:42:45 by acosi            ###   ########.fr       */
+/*   Created: 2024/06/30 23:12:02 by acosi             #+#    #+#             */
+/*   Updated: 2024/07/01 01:17:17 by acosi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FIXED_H
-#define FIXED_H
+#include "Fixed.hpp"
 
-#include <iostream>
-
-class Fixed
+int main(void)
 {
-public:
-	Fixed(void);
-	Fixed(const Fixed &src);	// Copy constructor
-	~Fixed(void);
-	
-	Fixed& operator=(const Fixed &rhs);	// Copy assignment operator overload
-	int getRawBits(void) const;
-	void setRawBits(int const raw);
-	int getFpValue(void) const;
-	
-private:
-	int _fpValue;
-	const static int _fbits = 8;
-};
+    Fixed a;
+	Fixed b(a);
+	Fixed c;
 
-#endif
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return 0;
+}

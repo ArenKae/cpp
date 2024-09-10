cd0d73b6497d161a1dde506e0df0c2184acdcefe
acosi <acosi@c1r1p3.42nice.fr>
2024-09-10 07:50:15 +0200

    Init

--- modules/cpp-05/ex00/Bureaucrat.cpp
+++ modules/cpp-05/ex00/Bureaucrat.cpp
@@ -6,7 +6,16 @@
 /*   By: acosi <acosi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 07:36:13 by acosi             #+#    #+#             */
-/*   Updated: 2024/09/10 07:36:14 by acosi            ###   ########.fr       */
+/*   Updated: 2024/09/10 07:50:10 by acosi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+// Default Constructor
+Bureaucrat::Bureaucrat(void)
+    : _name("Default name"), _grade(0)
+{
+    std::cout << "Bureaucrat default constructor called." << std::endl;
+}
 
--- modules/cpp-05/ex00/Bureaucrat.hpp
+++ modules/cpp-05/ex00/Bureaucrat.hpp
@@ -6,7 +6,40 @@
 /*   By: acosi <acosi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 07:36:16 by acosi             #+#    #+#             */
-/*   Updated: 2024/09/10 07:36:17 by acosi            ###   ########.fr       */
+/*   Updated: 2024/09/10 07:49:02 by acosi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+class Bureaucrat
+{
+	public:
+		// Default Constructor
+		Bureaucrat(void);
+	
+		// Name Constructor
+		Bureaucrat(const std::string &name);
+
+		// Copy Constructor
+		Bureaucrat(const Bureaucrat &src);
+
+		// Destructor
+		~Bureaucrat();
+
+		// Assignment Operator
+		Bureaucrat& operator=(const Bureaucrat &src);
+
+		// Member functions
+		void getName();
+		void getRange();
+
+	private:
+	const std::string _name;
+	int _grade;
+};
+
+#endif

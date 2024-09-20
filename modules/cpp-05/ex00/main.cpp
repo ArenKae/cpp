72b00e3558393e8e58b50cbb037ea48774badb42
ArenKae <cosianthony@gmail.com>
2024-09-20 03:43:44 +0200

    cleaning main for eval

--- modules/cpp-05/ex00/main.cpp
+++ modules/cpp-05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: acosi <acosi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 07:23:49 by acosi             #+#    #+#             */
-/*   Updated: 2024/09/16 23:47:54 by acosi            ###   ########.fr       */
+/*   Updated: 2024/09/20 03:43:03 by acosi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main(void)
 {
 	try { // The "try" keword indicates that this block is subject to exception handling.
 		Bureaucrat b1("Palpatine", 2);
-		std::cout << b1 << std::endl;
+		std::cout << BLUE << b1.getName() << RESET ", bureaucrat grade " GREEN << b1.getGrade() << RESET "." << std::endl; 
 		b1.incrementGrade();
 		std::cout << b1 << std::endl;
 		b1.incrementGrade(); // Throws GradeTooHighException
@@ -26,14 +26,16 @@ int main(void)
 	} catch (std::exception &e) {
 		// Calling the what() method to get the error from the exception
 		std::cerr << RED "Error: " << e.what() << RESET << std::endl; }
-		std::cout << std::endl;
 
+	std::cout << "\n---------------------------------------------\n" << std::endl;
+	
 	try {
 		Bureaucrat b2("Valorum", 151); // Throws GradeTooLowException
 	} catch (std::exception &e) {
 		std::cerr << RED "Error: " << e.what() << RESET << std::endl; }
-		std::cout << std::endl;
 
+	std::cout << "\n---------------------------------------------\n" << std::endl;
+	
 	try {
 		Bureaucrat b3("Nute Gunray", 150);
 		std::cout << b3 << std::endl;

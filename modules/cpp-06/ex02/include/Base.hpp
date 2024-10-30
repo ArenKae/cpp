30e695e3ed46b98adf79a012cc5a59ddc8a84c9f
acosi <cosianthony@gmail.com>
2024-10-30 16:51:48 +0100

    ex02 start

---
+++ modules/cpp-06/ex02/include/Base.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acosi <acosi@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 16:32:18 by acosi             #+#    #+#             */
+/*   Updated: 2024/10/30 16:34:15 by acosi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BASE_HPP
+#define BASE_HPP
+
+class Base
+{
+	public:
+		virtual ~Base();	// Public virtual destructor to allow dynamic casting
+};
+
+#endif
---
+++ modules/cpp-06/ex02/src/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acosi <acosi@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 03:30:55 by acosi             #+#    #+#             */
+/*   Updated: 2024/10/30 16:36:31 by acosi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "include/Base.hpp"
+#include "include/A.hpp"
+#include "include/B.hpp"
+#include "include/C.hpp"
+#include "include/utils.h"
+#include <iostream>
+
+int main()
+{
+	
+
+	return 0;
+}

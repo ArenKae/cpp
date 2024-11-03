eda82430da1eccc8c8d7a6a63761e684af1492be
acosi <cosianthony@gmail.com>
2024-11-03 17:25:04 +0100

    ex01 done

--- modules/cpp-07/ex01/iter.hpp
+++ modules/cpp-07/ex01/iter.hpp
@@ -6,18 +6,40 @@
 /*   By: acosi <acosi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 12:24:07 by acosi             #+#    #+#             */
-/*   Updated: 2024/11/03 16:08:07 by acosi            ###   ########.fr       */
+/*   Updated: 2024/11/03 16:54:20 by acosi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 #define ITER_HPP
 
-template<typename T>
-void iter(T &x,T &y)
-{
+#include <iostream>
 
+template<typename type, typename function>
+void iter(type *array, size_t len, function F)
+{
+	for (size_t i = 0; i < len; ++i)
+		F(array[i]);
 };
 
+/*   TEST FUNCTIONS   */
+
+template<typename type>
+void printElement(type &E)
+{
+	std::cout << E << " ";
+};
+
+template<typename type>
+void incrementElement(type &E)
+{
+	++E;
+};
+
+template<typename type>
+void to_42(type &E)
+{
+	E = 42;
+};
 
 #endif
--- modules/cpp-07/ex01/main.cpp
+++ modules/cpp-07/ex01/main.cpp
@@ -6,16 +6,86 @@
 /*   By: acosi <acosi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 11:59:35 by acosi             #+#    #+#             */
-/*   Updated: 2024/11/03 16:08:36 by acosi            ###   ########.fr       */
+/*   Updated: 2024/11/03 17:23:16 by acosi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 #include "utils.h"
-#include <iostream>
+
+void capitalizeChar(char &c)
+{
+	c = std::toupper(c);
+};
+
+void capitalizeStr(std::string &str)
+{
+	for (std::string::iterator i = str.begin(); i != str.end(); i++)
+		*i = std::toupper(*i);
+};
+
+void	appendSuffix(std::string &str)
+{
+	str += "_S";
+}
 
 int main(void)
 {
-	
+	{
+		int tab[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		size_t size = 10;
+		
+		std::cout << MAGENTA "[INT ARRAY TESTS]" RESET << std::endl;
+		::iter(tab, size, ::printElement<int>);
+
+		std::cout << YELLOW "\nIncrementing array :" RESET << std::endl;
+		::iter(tab, size, ::incrementElement<int>);
+		::iter(tab, size, ::printElement<int>);
+
+		std::cout << YELLOW "\n42-fying array :" RESET << std::endl;
+		::iter(tab, size, ::to_42<int>);
+		::iter(tab, size, ::printElement<int>);
+		std::cout << "\n" << std::endl;
+	}
+	{
+		char tab[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+		size_t size = 10;
+		
+		std::cout << MAGENTA "[CHAR ARRAY TESTS]" RESET << std::endl;
+		::iter(tab, size, ::printElement<char>);
+
+		std::cout << YELLOW "\nIncrementing array :" RESET << std::endl;
+		::iter(tab, size, ::incrementElement<char>);
+		::iter(tab, size, ::printElement<char>);
+
+		std::cout << YELLOW "\nCapitalizing array :" RESET << std::endl;
+		::iter(tab, size, capitalizeChar);
+		::iter(tab, size, ::printElement<char>);
+
+		std::cout << YELLOW "\n42-fying array :" RESET << std::endl;
+		::iter(tab, size, ::to_42<char>);
+		::iter(tab, size, ::printElement<char>);
+		std::cout << "\n" << std::endl;
+	}
+	{
+		std::string tab[] = {"I", "am", "your", "father"};
+		size_t size = 4;
+		
+		std::cout << MAGENTA "[STRING ARRAY TESTS]" RESET << std::endl;
+		::iter(tab, size, ::printElement<std::string>);;
+
+		std::cout << YELLOW "\nCapitalizing array :" RESET << std::endl;
+		::iter(tab, size, capitalizeStr);
+		::iter(tab, size, ::printElement<std::string>);
+
+		std::cout << YELLOW "\nAdding suffix to array :" RESET << std::endl;
+		::iter(tab, size, appendSuffix);
+		::iter(tab, size, ::printElement<std::string>);
+
+		std::cout << YELLOW "\n42-fying array :" RESET << std::endl;
+		::iter(tab, size, ::to_42<std::string>);
+		::iter(tab, size, ::printElement<std::string>);
+		std::cout << std::endl;
+	}
 	return 0;
 }

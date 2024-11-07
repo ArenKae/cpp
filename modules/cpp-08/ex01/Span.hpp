1dcba53f3169ad6cbe6e892702ba30053b53c2f4
acosi <acosi@c2r9p3.42nice.fr>
2024-11-07 11:33:42 +0100

    Canonical

--- modules/cpp-08/ex01/Span.hpp
+++ modules/cpp-08/ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: acosi <acosi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 11:24:06 by acosi             #+#    #+#             */
-/*   Updated: 2024/11/07 11:24:36 by acosi            ###   ########.fr       */
+/*   Updated: 2024/11/07 11:32:11 by acosi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,29 @@
 
 #include <iostream>
 
+class Span
+{
+	public:
+		// Default constructor #OrthodoxCanoncialForm
+		Span(void);
+		
+		// Value constructor
+		Span(unsigned int &N);
+		
+		// Copy constructor #OrthodoxCanoncialForm
+		Span(const Span &src);
+
+		// Destructor #OrthodoxCanoncialForm
+		~Span();
+
+		// Overloaded assignment operator #OrthodoxCanoncialForm
+		Span& operator=(const Span &src);
+
+		// Member functions
+		void addNumber();
+	private:
+		
+};
 
 
 #endif

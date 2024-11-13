961a0af20a15256be7405e2acc390417e2b7d720
acosi <cosianthony@gmail.com>
2024-11-13 04:05:10 +0100

    ex02 done

--- modules/cpp-08/ex02/MutantStack.hpp
+++ modules/cpp-08/ex02/MutantStack.hpp
@@ -6,7 +6,84 @@
 /*   By: acosi <acosi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 21:31:32 by acosi             #+#    #+#             */
-/*   Updated: 2024/11/12 21:31:33 by acosi            ###   ########.fr       */
+/*   Updated: 2024/11/13 04:02:50 by acosi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MUTANT_STACK_HPP
+#define MUTANT_STACK_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include "utils.h"
+
+/*	std::stack is a container adapter that provides a Last In, First Out (LIFO) data structure, 
+	meaning the last element added to the stack will be the first one removed. As an adapter, it is 
+	not a standalone container, but it's built ont top of one to adapt it (deque is used by default). */
+
+template <typename T>
+class MutantStack : public std::stack<T> // Inerit from stack to access functions like push, pop...
+{
+	public:
+		// Default constructor #OrthodoxCanoncialForm
+		// Initialize an empty stack.
+		MutantStack(void) : std::stack<T>() {};
+		
+		// Copy constructor #OrthodoxCanoncialForm
+		// Use the copy constructor from std::stack to copy the internal container of src.
+		MutantStack(const MutantStack<T> &src) : std::stack<T>(src) {};
+
+		// Destructor #OrthodoxCanoncialForm
+		~MutantStack() {};
+
+		// Overloaded assignment operator #OrthodoxCanoncialForm
+		// Use the assignment operator from std::stack to copy the internal container of src int this*.
+		MutantStack& operator=(const MutantStack<T> &src)
+		{
+			if (this != &src)
+				std::stack<T>::operator=(src);
+			return *this;
+		}
+	
+		// Define the iterator aliases for readability
+		typedef	typename std::stack<T>::container_type::iterator iterator;
+		typedef	typename std::stack<T>::container_type::const_iterator const_iterator;
+
+		/*	this->c is a protected member of std::stack representing the underlying container.
+			We can then access its own begin() and end() members to allow iteration. */
+		iterator begin() { return this->c.begin();};
+		iterator end() { return this->c.end();};
+};
+
+// Template helper function for printContainer
+template <typename T>
+struct PrintFunction {
+    void operator()(const typename T::value_type& item) const {
+        std::cout << item << " ";
+    }
+};
+
+// Template function to print the content of a container
+template <typename T>
+void printContainer(const T& container, const std::string &name) {
+	std::cout << BLUE << name << RESET " = [ ";
+    std::for_each(container.begin(), container.end(), PrintFunction<T>());
+    std::cout << "]" << std::endl;
+}
+
+// Template function to print Stack infos and make the main program more readable
+template <typename T>
+void printStackInfos(const T& container, const std::string &name) {
+	std::cout << BLUE << name << RESET " size is " GREEN << container.size() << RESET 
+	" and the last element is " YELLOW << container.top() << RESET << std::endl;
+}
+
+// Template function to print list infos and make the main program more readable
+template <typename T>
+void printListInfos(const T& container, const std::string &name) {
+	std::cout << BLUE << name << RESET " size is " GREEN << container.size() << RESET 
+	" and the last element is " YELLOW << container.back() << RESET << std::endl;
+}
+
+#endif
--- modules/cpp-08/ex02/main.cpp
+++ modules/cpp-08/ex02/main.cpp
@@ -6,15 +6,95 @@
 /*   By: acosi <acosi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 07:23:01 by acosi             #+#    #+#             */
-/*   Updated: 2024/11/12 21:31:12 by acosi            ###   ########.fr       */
+/*   Updated: 2024/11/13 04:04:23 by acosi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <list>
 #include "MutantStack.hpp"
 #include "utils.h"
 
+
 int main()
 {
+	// Tests for basic functionalities of the MutantStack class.
+	std::cout << MAGENTA "[MUTANT STACK TESTS]" RESET << std::endl;
+	MutantStack<int> mstack;
 	
+	mstack.push(5);
+	mstack.push(17);
+	printContainer(mstack, "mstack");
+	printStackInfos(mstack, "mstack");
+	
+	mstack.pop();
+	std::cout << RED "-> mstack last element poped!" RESET << std::endl;
+	printContainer(mstack, "mstack");
+	printStackInfos(mstack, "mstack");
+	
+	mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+	std::cout << RED "-> 4 elements were pushed to mstack..." RESET << std::endl;
+	printContainer(mstack, "mstack");
+	printStackInfos(mstack, "mstack");
+	
+	std::cout << RED "-> Creating iterator it..." RESET << std::endl;
+	MutantStack<int>::iterator it = mstack.begin();
+	std::cout << "*it = " << *it << std::endl;
+	std::cout << RED "-> Incrementing iterator it..." RESET << std::endl;
+	std::cout << "*it = " << *++it << std::endl;
+	std::cout << RED "-> Decrementing iterator it..." RESET << std::endl;
+	std::cout << "*it = " << *--it << std::endl;
+	
+	
+	std::cout << RED "-> Iterating through mstack using begin iterator :" RESET << std::endl;
+	for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); ++it)
+        std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << RED "-> Iterating through mstack using end iterator :" RESET << std::endl;
+	for (MutantStack<int>::iterator ite = --mstack.end(); ite != --mstack.begin(); --ite)
+        std::cout << *ite << " ";
+	std::cout << std::endl;
+
+	// Same tests with a std::list container to compare output
+	std::cout << MAGENTA "\n[LIST TESTS]" RESET << std::endl;
+	std::list<int> lst;
+	
+	lst.push_back(5);
+	lst.push_back(17);
+	printContainer(lst, "lst");
+	printListInfos(lst, "lst");
+	
+	lst.pop_back();
+	std::cout << RED "-> lst last element poped!" RESET << std::endl;
+	printContainer(lst, "lst");
+	printListInfos(lst, "lst");
+	
+	lst.push_back(3);
+    lst.push_back(5);
+    lst.push_back(737);
+    lst.push_back(0);
+	std::cout << RED "-> 4 elements were pushed to lst..." RESET << std::endl;
+	printContainer(lst, "lst");
+	printListInfos(lst, "lst");
+	
+	std::cout << RED "-> Creating iterator lit..." RESET << std::endl;
+	std::list<int>::iterator lit = lst.begin();
+	std::cout << "*lit = " << *lit << std::endl;
+	std::cout << RED "-> Incrementing iterator lit..." RESET << std::endl;
+	std::cout << "*lit = " << *++lit << std::endl;
+	std::cout << RED "-> Decrementing iterator lit..." RESET << std::endl;
+	std::cout << "*lit = " << *--lit << std::endl;
+	
+	std::cout << RED "-> Iterating through lst using begin iterator :" RESET << std::endl;
+	for (std::list<int>::iterator it = lst.begin(); it != lst.end(); ++it)
+        std::cout << *it << " ";
+	std::cout << std::endl;
+	std::cout << RED "-> Iterating through lst using end iterator :" RESET << std::endl;
+	for (std::list<int>::iterator ite = --lst.end(); ite != --lst.begin(); --ite)
+        std::cout << *ite << " ";
+	std::cout << std::endl;
+
     return 0;
 }

2f506adfe0508d1dc46a63ea58191d4d63a01f3d
acosi <cosianthony@gmail.com>
2024-11-27 03:19:50 +0100

    error handling

--- modules/cpp-09/ex02/PmergeMe.hpp
+++ modules/cpp-09/ex02/PmergeMe.hpp
@@ -6,7 +6,21 @@
 /*   By: acosi <acosi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 02:02:06 by acosi             #+#    #+#             */
-/*   Updated: 2024/11/27 02:02:06 by acosi            ###   ########.fr       */
+/*   Updated: 2024/11/27 02:12:38 by acosi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PMERGE_ME_HPP
+#define PMERGE_ME_HPP
+
+#include <iostream>
+#include <list>
+#include <vector>
+#include <cstdlib>		// atoi()
+#include <sstream>		// string stream manipulation
+#include <stdexcept>
+
+
+
+
+#endif
--- modules/cpp-09/ex02/main.cpp
+++ modules/cpp-09/ex02/main.cpp
@@ -6,16 +6,43 @@
 /*   By: acosi <acosi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 07:23:01 by acosi             #+#    #+#             */
-/*   Updated: 2024/11/27 02:02:57 by acosi            ###   ########.fr       */
+/*   Updated: 2024/11/27 02:53:49 by acosi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 #include "utils.h"
 
+static bool isPositiveInt(const std::string &arg)
+{
+	for (size_t i = 0; i < arg.length(); ++i)
+		if (!isdigit(arg[i]))
+			return false;
+	return !arg.empty(); // Return false if arg is empty, true otherwise.
+}
+
 int main(int ac, char **av)
 {
+	if (ac < 2) {
+		std::cerr << RED "Error: No input provided." RESET << std::endl;
+		return 1; }
 
-	
-	return 0;
+	std::vector<int> vec;
+	std::list<int> lst;
+
+	try {
+		for (int i = 1; i < ac; ++i) {
+			if (!isPositiveInt(av[i]))
+				throw std::invalid_argument("Invalid input");
+			int num = std::atoi(av[i]);
+			vec.push_back(num);
+			lst.push_back(num); }
+		
+		// Display unsorted sequence
+		std::cout << "Before: ";
+        for (size_t i = 0; i < vec.size(); ++i) std::cout << vec[i] << " ";
+        std::cout << std::endl;
+	}
+	catch (const std::exception& e) {
+        std::cerr << RED "Error: " << e.what() << RESET << std::endl; }
 }
